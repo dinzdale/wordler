c919526a6247d879ffaafdc3754fa324302ff6a2
dinzdale <gmjacobs61@gmail.com>
2023-12-12 12:58:10 -0500

    Render according to definition results.

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -2,7 +2,10 @@ import Network.WordlerAPI
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
@@ -19,6 +22,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.unit.dp
 import gameboard.TileRow
 import model.ui.game_pieces.TileData
 import model.ui.game_pieces.TileStatus
@@ -44,21 +49,31 @@ fun ShowLayout() {
 @Composable
 fun ShowGameBoard() {
     val words = remember { mutableStateListOf("ABCDE") }
-    var definition by remember { mutableStateOf("") }
+    var definition by remember { mutableStateOf<String?>(null) }
     var cnt by remember { mutableStateOf(0) }
 
     val scrollState = rememberScrollState()
 
 
-    fun getMockTileDataList(word: String): List<TileData> {
+    fun getMockTileDataList(word: String, definition:String?): List<TileData> {
         val charArray = word.toCharArray()
-        return listOf(
-            TileData(charArray[0], TileStatus.MATCH_IN_POSITION, 0),
-            TileData(charArray[1], TileStatus.MATCH_OUT_POSITION, 1),
-            TileData(charArray[2], TileStatus.MATCH_IN_POSITION, 2),
-            TileData(charArray[3], TileStatus.NO_MATCH, 3),
-            TileData(charArray[4], TileStatus.NO_MATCH, 4),
-        )
+        if (charArray.isNotEmpty() && definition != null) {
+            return listOf(
+                TileData(charArray[0], TileStatus.MATCH_IN_POSITION, 0),
+                TileData(charArray[1], TileStatus.MATCH_OUT_POSITION, 1),
+                TileData(charArray[2], TileStatus.MATCH_IN_POSITION, 2),
+                TileData(charArray[3], TileStatus.NO_MATCH, 3),
+                TileData(charArray[4], TileStatus.NO_MATCH, 4),
+            )
+        } else {
+            return listOf(
+                TileData('X', TileStatus.EMPTY, 0),
+                TileData('X', TileStatus.EMPTY, 1),
+                TileData('X', TileStatus.EMPTY, 2),
+                TileData('X', TileStatus.EMPTY, 3),
+                TileData('X', TileStatus.EMPTY, 4),
+            )
+        }
     }
     LaunchedEffect(cnt) {
         WordlerAPI.getWords().apply {
@@ -67,30 +82,35 @@ fun ShowGameBoard() {
                 words.addAll(wordList.map { it.uppercase() })
                 WordlerAPI.getDictionaryDefinition(wordList[0]).apply {
                     onSuccess { itemsList ->
-                        definition = itemsList[0].toString()
+                        definition = itemsList[0].meanings[0].definitions[0].definition
                     }
                     onFailure {
-                        definition = "no definition: ${it.message}"
+                        definition = null
                     }
                 }
             }
             onFailure {
-                definition = "word list failed: ${it.message}"
+                definition = null
             }
         }
     }
     Box(
         modifier = Modifier.fillMaxSize().verticalScroll(scrollState),
-        contentAlignment = Alignment.TopCenter
+        contentAlignment = Alignment.Center
     ) {
         Column(
             modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.SpaceEvenly,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            TileRow(getMockTileDataList(words[0]))
+            TileRow(getMockTileDataList(words[0],definition))
+            Spacer(Modifier.height(5.dp))
             Text(words.joinToString())
-            Text(definition)
+            Spacer(Modifier.height(5.dp))
+            definition?.also {
+                Text(it, modifier = Modifier.padding(horizontal = 5.dp))
+            }
+            Spacer(Modifier.height(20.dp))
             Button({
                 cnt = ++cnt % 2
             }) {
