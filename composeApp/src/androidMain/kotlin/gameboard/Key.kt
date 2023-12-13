89c797e92103146acd6a46a8a067ae428bf192f8
dinzdale <gmjacobs61@gmail.com>
2023-12-13 15:34:19 -0500

    Initial AlphaKey

--- composeApp/src/androidMain/kotlin/gameboard/Key.kt
+++ composeApp/src/androidMain/kotlin/gameboard/Key.kt
@@ -1,34 +1,62 @@
 package gameboard
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
-import androidx.compose.material.ButtonColors
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import model.ui.game_pieces.KeyData
 import model.ui.game_pieces.KeyType
-import model.ui.game_pieces.PieceColor.Companion.getColor
+import model.ui.game_pieces.PieceColor
+import model.ui.game_pieces.TileKeyStatus
 
 @Composable
-fun Key(keyData: KeyData, onKeySelection:(KeyType,Char?)->Unit) {
-    when(keyData.keyType) {
+fun Key(keyData: KeyData, onKeySelection: (KeyData) -> Unit) {
+    when (keyData.keyType) {
         // ALPHA, ENTER, RETURN
         KeyType.ALPHA -> {}
         KeyType.ENTER -> {}
-        KeyType.RETURN ->{}
+        KeyType.RETURN -> {}
     }
 }
 
 @Composable
-fun KeyShowAlphaKey(keyData: KeyData, onKeySelection:(KeyType,Char?)->Unit) {
+fun AlphaKey(keyData: KeyData, onKeySelection: (KeyData) -> Unit) {
+    val (backGround, foreGround) = PieceColor.getColor(keyData)
+    Button(
+        { onKeySelection(keyData) },
+        Modifier.width(36.dp).height(48.dp),
+        colors = ButtonDefaults.buttonColors(backgroundColor = backGround),
 
-    Button( {onKeySelection(keyData.keyType,keyData.char)},Modifier.height(20.dp).width(10.dp).background(color = getColor(keyData).backGround)) {
-
+        ) {
+        Text("Q", textAlign = TextAlign.Center, style = TextStyle(color = foreGround, fontWeight = FontWeight.Bold, fontSize = 24.sp))
     }
 }
+
+@Composable
+@Preview
+fun PreviewAlphaKey() {
+    MaterialTheme {
+        Surface(Modifier.fillMaxSize()) {
+            Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                AlphaKey(keyData = KeyData('Q', KeyType.ALPHA, TileKeyStatus.MATCH_IN_POSITION)) {
+
+                }
+            }
+        }
+    }
+}
+
--- composeApp/src/commonMain/kotlin/model/ui/game_pieces/TileProps.kt
+++ composeApp/src/commonMain/kotlin/model/ui/game_pieces/TileProps.kt
@@ -3,15 +3,18 @@ package model.ui.game_pieces
 import androidx.compose.ui.graphics.Color
 
 enum class TileKeyStatus {
-    INITIAL_KEY,  NO_MATCH, MATCH_IN_POSITION, MATCH_OUT_POSITION, EMPTY,
+    INITIAL_KEY, NO_MATCH, MATCH_IN_POSITION, MATCH_OUT_POSITION, EMPTY,
 }
 
 enum class PieceColor(val backGround: Color, val foreGround: Color) {
     EMPTY(Color.Black, Color.Black),
-    INITIAL_KEY(Color(red=102, 107, 100),Color.White),
+    INITIAL_KEY(Color(red = 102, 107, 100), Color.White),
     MATCH_IN_POSITION(Color(red = 70, 235, 52), Color.White),
     MATCH_OUT_POSITION(Color(red = 235, 204, 52), Color.White),
     NO_MATCH(Color.Black, Color.White);
+
+    operator fun component1() = backGround
+    operator fun component2() = foreGround
 
     companion object {
         fun getColor(tileKey: TileKeyData): PieceColor {
@@ -35,12 +38,14 @@ enum class KeyType {
 }
 
 sealed class TileKeyData()
-data class TileData(val char: Char, val status: TileKeyStatus, val columnPosition: Int = 0) : TileKeyData()
+data class TileData(val char: Char, val status: TileKeyStatus, val columnPosition: Int = 0) :
+    TileKeyData()
 
-data class KeyData(val char: Char?=null, val keyType: KeyType, val status: TileKeyStatus) : TileKeyData()
+data class KeyData(val char: Char? = null, val keyType: KeyType, val status: TileKeyStatus) :
+    TileKeyData()
 
 
-data class RowData(val tileData:List<TileData>, val rowPosition: Int = 0)
+data class RowData(val tileData: List<TileData>, val rowPosition: Int = 0)
 
 
 
