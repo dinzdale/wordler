98ef518a88f38e217167ffc9aa24ea280c24f37a
dinzdale <gmjacobs61@gmail.com>
2023-12-28 12:53:20 -0500

    Got rotation working .. fix rotation.

--- composeApp/src/commonMain/kotlin/gameboard/Tile.kt
+++ composeApp/src/commonMain/kotlin/gameboard/Tile.kt
@@ -3,13 +3,15 @@ package gameboard
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -18,20 +20,53 @@ import model.ui.game_pieces.TileData
 import model.ui.game_pieces.TileKeyStatus
 
 @Composable
-fun Tile(angle:Float,tileData: TileData = TileData('Q',TileKeyStatus.EMPTY)) {
+fun Tile(angle: Float, tileData: TileData = TileData('Q', TileKeyStatus.EMPTY)) {
+    var previousTile by remember { mutableStateOf(tileData) }
+    if (tileData.status != TileKeyStatus.MATCH_OUT_POSITION && tileData.status != TileKeyStatus.MATCH_IN_POSITION) {
+        previousTile = tileData
+    }
+    if (angle <= 90f) {
+        RenderTile(angle,previousTile)
+    }
+    else {
+        RenderTile(angle,tileData)
+    }
+//    Box(
+//        Modifier
+//            .graphicsLayer {
+//                rotationY = angle
+//            }
+//            .background(color = PieceColor.getColor(tileData).backGround)
+//            .size(size = 70.dp),
+//        contentAlignment = Alignment.Center
+//    ) {
+//        Text(
+//            tileData.char.toString(),
+//            color = PieceColor.getColor(tileData).foreGround,
+//            style = TextStyle(fontSize = 38.sp)
+//        )
+//    }
+}
+
+@Composable
+private fun RenderTile(angle: Float, tileData: TileData) {
+    val (background,foreground) = PieceColor.getColor(tileData)
     Box(
         Modifier
             .graphicsLayer {
                 rotationY = angle
             }
-            .background(color = PieceColor.getColor(tileData).backGround)
+            .background(color = background)
             .size(size = 70.dp),
         contentAlignment = Alignment.Center
     ) {
-        Text(tileData.char.toString(), color = PieceColor.getColor(tileData).foreGround, style = TextStyle(fontSize = 38.sp))
+        Text(
+            tileData.char.toString(),
+            color = foreground,
+            style = TextStyle(fontSize = 38.sp)
+        )
     }
 }
-
 //@Composable
 //@Preview
 //fun PreviewTileInitial() {
