bf691286e68a71fc67004e1c56be2376c2558da8
dinzdale <gmujacobs61@gmail.com>
2025-01-25 17:45:33 -0500

    version updates

---
+++ composeApp/src/commonMain/kotlin/gameboard/WordHint.kt
@@ -0,0 +1,30 @@
+package gameboard
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import kotlinx.coroutines.FlowPreview
+
+@Composable
+fun WordHint(showHint: Boolean, word: String, onDismiss: () -> Unit) {
+    if (showHint) {
+        Box(Modifier.fillMaxSize(.5f), contentAlignment = Alignment.Center) {
+            Dialog(onDismiss) {
+                Text(word, style = TextStyle(fontSize = 24.sp))
+            }
+        }
+    }
+}
+
+
+
+
+
