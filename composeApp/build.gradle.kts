4b1845ce906856b636ff3f10ede02cf9acfed97e
gjacobs <gjacobs@mdsol.com>
2025-01-29 13:24:52 -0500

    more string to resource migration.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -73,6 +73,7 @@ kotlin {
             implementation(libs.ktor.client.core)
             implementation(libs.ktor.client.content.negotiation)
             implementation(libs.ktor.serialization.kotlinx.json)
+            implementation(libs.navigation.compose)
         }
         iosMain {
             // ...
