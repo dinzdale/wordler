25b1c279823f9460373c496815e298ed0fc77845
dinzdale <gjacobs61@gmail.com>
2026-01-04 21:25:18 -0500

    cleanup

--- composeApp/src/commonMain/kotlin/gameboard/GameBoard.kt
+++ composeApp/src/commonMain/kotlin/gameboard/GameBoard.kt
@@ -1,6 +1,5 @@
 package gameboard
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
